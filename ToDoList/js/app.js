e145550babe13cf91d8584b0fe684959edaa646d
SebastianHajdus <sebastian.hajdus@gmail.com>
2016-11-07 00:00:11 +0100

    eventy

--- ToDoList/js/app.js
+++ ToDoList/js/app.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(e) {
 
+    console.log('dziala');
+
+    var addTodoInput = document.querySelector('#todo-input'),
+        addTodoBtn = document.querySelector('#add-todo-btn');
+
+    addTodoBtn.addEventListener('click', function(e) {
+        var inputValue = addTodoInput.value;
+        alert(inputValue);
+    });
+
+});
